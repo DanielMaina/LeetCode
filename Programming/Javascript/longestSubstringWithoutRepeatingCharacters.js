cfcdc019a2c42059789d37bbf1f928802d136601
DanielMaina <daniel_maina9@hotmail.com>
2021-09-28 14:39:06 -0700

    more problems added

---
+++ Programming/Javascript/longestSubstringWithoutRepeatingCharacters.js
@@ -0,0 +1,27 @@
+/** 
+ Given a string s, find the length of the longest substring without repeating characters.
+**/
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    
+    let res = 0;
+    let set = new Set();
+    let i = 0;
+    let j = 0;
+    
+      while (i < s.length && j < s.length) {
+        if (!set.has(s[j])) {
+          set.add(s[j]);
+          j++;
+          if (j - i > res) res = j - i;
+        } else {
+          set.delete(s[i]);
+          i++;
+        }
+      }
+  return res; 
+}
